8c3fab9b4858d6bb808f308e267da9ab9b55ee1d
TimelessAdventure <148522576+TimelessAdventure@users.noreply.github.com>
2025-02-20 22:47:28 +0530

    Update pymodoro.py

--- pymodoro.py
+++ pymodoro.py
@@ -1,6 +1,6 @@
 import time
-study=int(input("Enter the duration to study: "))
-relax=int(input("Enter the duration of break: "))
+study=int(input("Enter the duration to study (mins): "))
+relax=int(input("Enter the duration of break (mins): "))
 count=int(input("Enter the number of cycles: "))
 
 for i in range(0, count):
