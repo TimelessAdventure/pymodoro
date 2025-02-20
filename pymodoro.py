8c90c201e35fbcaefdfc88da643bfaaa5cfc9a96
TimelessAdventure <148522576+TimelessAdventure@users.noreply.github.com>
2025-02-20 23:32:29 +0530

    Update pymodoro.py
    Added countdown

--- pymodoro.py
+++ pymodoro.py
@@ -3,10 +3,21 @@ study=int(input("Enter the duration to study (mins): "))
 relax=int(input("Enter the duration of break (mins): "))
 count=int(input("Enter the number of cycles: "))
 
+def display(timed):
+    while (timev): 
+        mins, secs = divmod(timed, 60) 
+        timer = '{:02d}:{:02d}'.format(mins, secs) 
+        print(timer, end="\r") 
+        time.sleep(1) 
+        timed -= 1   
+
 for i in range(0, count):
-    time.sleep(study*60)
+    study=studyb
+    print("Time till break: ")
+    display(study)
     print("Break time! Yay!")
     print('\a')
-    time.sleep(relax*60)
+    print("Remaining break time: ")
+    relax=relaxb
+    display(relax)
     print("Get back to work")
-    print('\a')
